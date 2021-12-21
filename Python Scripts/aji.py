48d00e3adca2703fb809c6af7883db94bf3c6c17
ajmalghazzan <96407141+ajmalas123@users.noreply.github.com>
2021-12-21 11:39:35 +0530

    Add files via upload
    initial commit

---
+++ Python Scripts/aji.py
@@ -0,0 +1,7 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 18:46:30) [MSC v.1929 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========================= RESTART: C:/Users/user/aji.py ========================
+Enter colors (space between colors):red blue green
+['red', 'blue', 'green']
+the first color in the list is: red and last color is : green
